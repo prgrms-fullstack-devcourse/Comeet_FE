bd78e22a410bfab4c101e1cef7218e1cf32688d2
Monixc <chris13404@gmail.com>
2025-07-21 05:34:03 +0900

    style: GlobalLayout 컴포넌트 코드 정리
    - floating 버튼 정렬을 위해 relative 설정

--- src/components/layout/GlobalLayout.tsx
+++ src/components/layout/GlobalLayout.tsx
@@ -1,23 +1,19 @@
 import type { ReactNode } from "react";
 
 interface GlobalLayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 function GlobalLayout({ children }: GlobalLayoutProps) {
   return (
     <div className="min-h-screen bg-slate-100 flex justify-center">
-      <div className="w-[480px] bg-white flex flex-col">
-        <header className="w-full border-b border-border h-14">
-        </header>
-        <main className="flex-grow p-4">
-          {children}
-        </main>
-        <footer className="w-full  border-t border-border h-14">
-        </footer>
+      <div className="w-[480px] bg-white flex flex-col relative">
+        <header className="w-full border-b border-border h-14"></header>
+        <main className="flex-grow p-4">{children}</main>
+        <footer className="w-full  border-t border-border h-14"></footer>
       </div>
-    </div >
+    </div>
   );
-};
+}
 
 export default GlobalLayout;
