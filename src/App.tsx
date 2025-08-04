c627cac6dad2ab07eed833ee99ac62f9526f1e50
Monixc <chris13404@gmail.com>
2025-08-04 12:21:57 +0900

    feat: 리액트 쿼리 프로바이더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import GlobalLayout from "./components/layout/GlobalLayout";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <GlobalLayout>
-      <Outlet />
-    </GlobalLayout>
+    <QueryClientProvider client={queryClient}>
+      <GlobalLayout>
+        <Outlet />
+      </GlobalLayout>
+    </QueryClientProvider>
   );
 }
 
